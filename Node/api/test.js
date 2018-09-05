f2f863d94a10396d56004b84dcadf83274587c35
wensheng.xiao <wensheng.xiao@qunar.com>
2018-09-05 20:44:01 +0800

    add

---
+++ Node/api/test.js
@@ -0,0 +1,3 @@
+const assert = require('assert');
+
+assert.deepStrictEqual(String('1'), '1', 'true');
--- jsdoc/a.js
+++ jsdoc/a.js
@@ -1,3 +1,9 @@
-module.exports = {
-    obj: 1
-};
+var a = /123/g;
+for (const i in a) {
+    console.log(i);// 不执行
+}
+
+var b = { a };
+for (const i in b) {
+    console.log(i);// 执行
+}
